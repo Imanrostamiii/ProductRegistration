9a763d26dac54369857a34f0b852f28e4f0ec734
Imanrostami <imanrostami8219@gmail.com>
2024-04-07 15:05:40 +0330

    Write Dto class product

---
+++ ProductRegistration/Modele/Dto/ProductSelectDto.cs
@@ -0,0 +1,16 @@
+namespace ProductRegistration.Modele.Dto;
+
+public class ProductSelectDto
+{
+    public string NameProduct { get; set; }
+
+    public string Emile { get; set; }
+    
+    public string Phone { get; set; }
+    
+    public bool IsAvailable  { get; set; }
+    
+    public DateTime ProductDate { get; set; }
+
+
+}
