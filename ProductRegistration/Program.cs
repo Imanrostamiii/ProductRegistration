58dd44d2b1771efb9c8d65f7c1b772038ef2ca3d
Imanrostami <imanrostami8219@gmail.com>
2024-04-07 15:11:53 +0330

    Write Identity in program

--- ProductRegistration/Program.cs
+++ ProductRegistration/Program.cs
@@ -1,5 +1,8 @@
 using Data.Repositories;
 using Infrastructure.Data.DbContext;
+using Infrastructure.Entity.Role;
+using Infrastructure.Entity.User;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,6 +23,20 @@ builder.Services.AddDbContext<AppDbcontext>(option =>
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 #endregion
 
+#region Identity
+builder.Services.AddIdentity<loginUser, Role>(identityOptions =>
+    {
+        identityOptions.Password.RequireDigit = false;
+        identityOptions.Password.RequiredLength = 0;
+        identityOptions.Password.RequireNonAlphanumeric = false;
+        identityOptions.Password.RequireUppercase = false;
+        identityOptions.Password.RequireLowercase = false;
+
+        //UserName Settings
+        identityOptions.User.RequireUniqueEmail = false;
+    }).AddEntityFrameworkStores<AppDbcontext>()
+    .AddDefaultTokenProviders();
+#endregion
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
