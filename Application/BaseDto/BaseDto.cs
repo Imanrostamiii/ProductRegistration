2130d51f2df2626f21bccd42a872c1c3cbf3a268
Imanrostami <imanrostami8219@gmail.com>
2024-04-08 10:16:15 +0330

    Add basedto and Mapper

---
+++ Application/BaseDto/BaseDto.cs
@@ -0,0 +1,67 @@
+﻿using System.ComponentModel.DataAnnotations;
+using AutoMapper;
+using Infrastructure.Entity.BaseEntity;
+
+
+namespace Application.BaseDto;
+
+public class BaseDto<TDto, TEntity, TKey> : IHaveCustomMapping
+{
+    [Key] public TKey Id { get; set; }
+
+    public virtual TEntity ToEntity()
+    {
+        var config = new MapperConfiguration(cfg =>
+        {
+            cfg.AddMaps(typeof(CustomMappingProfile).Assembly);
+        });
+        var mapper = config.CreateMapper();
+        return mapper.Map<TEntity>(this);
+    }
+    public TEntity ToEntity(TEntity entity)
+    {
+        var config = new MapperConfiguration(cfg =>
+        {
+            cfg.AddMaps(typeof(CustomMappingProfile).Assembly);
+        });
+        var mapper = config.CreateMapper();
+        return mapper.Map(CastToDerivedClass(this), entity);
+    }
+ 
+    protected TDto CastToDerivedClass(BaseDto<TDto, TEntity, TKey> baseInstance)
+    {
+        var config = new MapperConfiguration(cfg =>
+        {
+            cfg.AddMaps(typeof(CustomMappingProfile).Assembly);
+        });
+        var mapper = config.CreateMapper();
+        return mapper.Map<TDto>(baseInstance);
+    }
+
+
+    public virtual void ApplyMapping(Profile profile)
+    {
+        var mappingExpression = profile.CreateMap<TDto, TEntity>();
+
+        var dtoType = typeof(TDto);
+        var entityType = typeof(TEntity);
+        //Ignore any property of source (like Post.Author) that dose not contains in destination 
+        foreach (var property in entityType.GetProperties())
+        {
+            if (dtoType.GetProperty(property.Name) == null)
+                mappingExpression.ForMember(property.Name, opt => opt.Ignore());
+        }
+
+        CustomMappings(mappingExpression.ReverseMap());
+    }
+    public virtual void CustomMappings(IMappingExpression<TEntity, TDto> mapping)
+    {
+    }
+}
+
+public abstract class BaseDto<TDto, TEntity> : BaseDto<TDto, TEntity, int>
+    where TDto : class, new()
+    where TEntity : BaseEntity<int>, new()
+{
+
+}
---
+++ Application/mapping/CustomMappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Application;
+
+public class CustomMappingProfile : Profile
+{
+    public class CustomMapping : Profile
+    {
+        public CustomMapping()
+        {
+            var allTypes = Assembly.GetEntryAssembly()!.GetExportedTypes();
+            var dtoTypes = allTypes.Where(p =>
+                p is { IsAbstract: false, IsClass: true, IsPublic: true } && p.GetInterfaces().Contains(typeof(IHaveCustomMapping)));
+            foreach (var type in dtoTypes)
+            {
+                var createMapping = Activator.CreateInstance(type) as IHaveCustomMapping;
+                createMapping!.ApplyMapping(this);
+            }
+        }
+    }
+}
